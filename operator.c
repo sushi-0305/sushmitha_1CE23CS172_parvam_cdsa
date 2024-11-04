9f411330e28490c3d4af618800a609d93895de22
sushi-0305 <sushmithap0305@gmail.com>
2024-11-04 12:35:57 +0530

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=3,b=3,sum,pro,diff;
+    sum=a+b;
+    diff=a-b;
+    pro=a*b;
+    printf("sum= %d\ndifference= %d\nproduct= %d\n",sum,diff,pro);
+    return 0;
+}

12b86261b321a2272dbcc818e3f1b243be3758a4
sushi-0305 <sushmithap0305@gmail.com>
2024-11-04 14:59:49 +0530

    Add files via upload

---
+++ logical.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a=10,b=20;
+    if((a>5)&&(b>5)){
+        printf("a & b are greater than 5\n");
+    }
+    else{
+        printf("a & b are not greater than 5\n");
+    }
+    if((a>5)||(b>5)){
+        printf("a & b are greater than 5");
+    }
+    else{
+        printf("a & b are not greater than 5");
+    }
+    }
+    
